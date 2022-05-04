ad2b3f9e2a2f02286a2ed0d7d0a4ad5a3ea5afcd
Guillaume <guillaume@deepidoo.com>
2022-05-04 11:01:10 +0200

    Messing around

--- app/javascript/views/musicians/index.vue
+++ app/javascript/views/musicians/index.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="uk-container">
+  <div class="container">
     <h1>{{ $t('home.title') }}</h1>
 
     <ul class="uk-breadcrumb uk-margin uk-margin-top">
       <li class="breadcrumb-item"><span>{{ $t('home.breadcrumb') }}</span></li>
     </ul>
 
-    <ul>
+    <!-- <ul>
       <li v-for="musician in store.musicians" :key="musician.id">
         <router-link :to="{ name: 'musician_path', params: {id: musician.id}}">{{ musician.name }}</router-link>
       </li>
-    </ul>
-
+    </ul> -->
+    
+    <div class="row">
+      <div class="col-xs-24 col-md-4"></div>
+      <div class="col-xs-24 col-md-4"></div>
+      <div class="col-xs-24 col-md-4"></div>
+      <div class="col-xs-24 col-md-4 col-start-md-18"></div>
+      <div class="col-xs-24 col-md-4"></div>
+      <div class="col-xs-24 col-md-4"></div>
+      <div class="col-xs-24 col-md-4"></div>
+      <div class="col-xs-24 col-md-4"></div>
+      <div class="col-xs-24 col-md-4"></div>
+    </div>
   </div>
 </template>
 
